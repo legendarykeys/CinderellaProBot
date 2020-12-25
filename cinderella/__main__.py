cfca3186d04d46d93c77683a9f509632c4522d85
mry0000 <74806581+mry0000@users.noreply.github.com>
2020-12-25 14:10:16 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -37,6 +37,7 @@ HELP_STRINGS = """
 Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
+
 *Main commands available*:
  🌀 - /start: start the bot
  🌀 - /help: PM's you this message.
