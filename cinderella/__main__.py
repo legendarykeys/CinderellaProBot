1b610f377ab78d1aa02bd1fe867487a93dc2b56c
mry0000 <74806581+mry0000@users.noreply.github.com>
2020-12-27 14:08:32 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -284,7 +284,9 @@ def get_help(bot: Bot, update: Update):
 
         update.effective_message.reply_text("Contact me in PM to get Instant help.",
                                             reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="Help",url="t.me/{}?start=help".format(bot.username))],    
+                                                [[InlineKeyboardButton(text="Help",url="t.me/{}?start=help".format(bot.username))],
+                                                [InlineKeyboardButton(text="🛡Contact Creator",url="https://t.me/Mr_Y00")]]))
+
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
