6f8e4f3ce244cf6192c274add5367e9e912edc4d
LEGENDARY KEYS <70282108+Keys-007@users.noreply.github.com>
2021-01-30 12:08:50 +0530

    Update special.py

--- cinderella/modules/special.py
+++ cinderella/modules/special.py
@@ -109,7 +109,7 @@ def leavechat(bot: Bot, update: Update, args: List[int]):
 def slist(bot: Bot, update: Update):
     message = update.effective_message
     text1 = "My Sudo Users are❤:"
-    text2 = "My Support Users are🤍:"
+    text2 = "My Support Users are⚡:"
     for user_id in SUDO_USERS:
         try:
             user = bot.get_chat(user_id)
