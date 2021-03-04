b5d151ac2d2b7222075ce89c27d63ab95b0247f1
LEGENDARY KEYS <70282108+Keys-007@users.noreply.github.com>
2021-03-04 15:47:24 +0530

    Test 3 fed

--- cinderella/modules/feds.py
+++ cinderella/modules/feds.py
@@ -2,5 +2,5 @@ __mod_name__ = "FEDERATIONS"
 
 __help__ = """Plz Ye Fed Ki Bakchodi Rose Tak Hi Rakho Feds Me Apni Gand Na Marvao
 ~ [my master](https://t.me/Denzid_xd),link_preview=False,
- 
+)
 """
