5b00f25dc505506d7684b2e7f818ec4be7c5a167
Anonymous Indian <73958752+anonyindian@users.noreply.github.com>
2021-03-22 01:43:58 +0530

    Update misc.py

--- cinderella/modules/helper_funcs/misc.py
+++ cinderella/modules/helper_funcs/misc.py
@@ -73,7 +73,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton(">>>", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("🏡 Home", callback_data="bot_start")]]
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
 
 
     return pairs
